5e76db5006c26f8aab8a9d0b94467d48a70135d5
SHEMA IRADUKUNDA Daniel <danieliradukunda00@gmail.com>
2025-08-26 13:40:23 +0200

    First commit

---
+++ alx-react-app/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import WelcomeMessage from "./components/WelcomeMessage";
+
+function App() {
+  return (
+    <>
+      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
