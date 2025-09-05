e976112e47c2e24774e62a9735ae41f35750318f
SHEMA IRADUKUNDA Daniel <danieliradukunda00@gmail.com>
2025-09-05 18:58:51 +0200

    renaming NavBar to Navbar

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -3,8 +3,8 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Services from "./components/Services";
 import Contact from "./components/Contact";
-import Navbar from "./components/NavBar";
 import Footer from "./components/Footer";
+import Navbar from "./components/NavBar";
 
 function App() {
   return (
