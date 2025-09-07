d9b765d8e3966601f54bf82712709381b564b1c3
SHEMA IRADUKUNDA Daniel <danieliradukunda00@gmail.com>
2025-09-07 21:40:48 +0200

    renaming the directory

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import Footer from "./components/Footer.jsx";
+import Header from "./components/Header.jsx";
+import MainContent from "./components/MainContent.jsx";
+import UserProfile from "./components/UserProfile.jsx";
+import WelcomeMessage from "./components/WelcomeMessage.jsx";
+
+function App() {
+  return (
+    <>
+      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    </>
+  );
+}
+
+export default App;
---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p style={{ color: "#7c7c7cff" }}>© 2023 City Lovers</p>
+    </footer>
+  );
+}
