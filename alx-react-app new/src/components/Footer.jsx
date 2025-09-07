7ef89382bfef6e187a04e6d2d9a0a86ad9a85ba2
SHEMA IRADUKUNDA Daniel <danieliradukunda00@gmail.com>
2025-09-07 21:45:25 +0200

    renaming the directory

--- alx-react-app new/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer>
-      <p style={{ color: "#7c7c7cff" }}>© 2023 City Lovers</p>
-    </footer>
-  );
-}
--- alx-react-app new/src/components/MainContent.jsx
+++
@@ -1,9 +0,0 @@
-export default function MainContent() {
-  return (
-    <main>
-      <p style={{ color: "red", textDecoration: "upperCase" }}>
-        I love to visit New York, Paris, and Tokyo.
-      </p>
-    </main>
-  );
-}
